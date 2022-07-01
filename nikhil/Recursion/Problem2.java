87c93afd83e490d68adee44fa5077f57bc018cf9
nikhilmangal60 <58554601+nikhilmangal60@users.noreply.github.com>
2022-07-01 12:03:47 +0530

    Alternate Problem2 Solution

--- nikhil/Recursion/Problem2.java
+++ nikhil/Recursion/Problem2.java
@@ -1,3 +1,5 @@
+//Using Collections.sort() to sort the list-
+//TC= O(n!n), SC= O(n)
 class Solution {
     public void createList(String S, String temp, Set<String> uniqueOutput)
     {
@@ -25,6 +27,38 @@ class Solution {
             output.add(temp);
         }
         Collections.sort(output);
+        return output;
+    }
+}
+
+//Use TreeSet to sort and remove duplicate in the list
+class Solution {
+    public void createList(String S, String temp, TreeSet<String> uniqueOutput)
+    {
+       
+        if(S.length()==0)
+        {
+            uniqueOutput.add(temp);
+            return;
+        }
+        
+        for(int i=0;i<S.length();i++)
+        {
+            String s1 = S.substring(0,i)+S.substring(i+1,S.length());
+            createList(s1,temp+S.charAt(i),uniqueOutput);
+        }
+        return;
+    }
+    
+    public List<String> find_permutation(String S) {
+        // Code here
+        TreeSet<String> uniqueOutput = new TreeSet<>();
+        createList(S,"",uniqueOutput);
+        List<String> output = new ArrayList<>();
+        for(String temp: uniqueOutput){
+            output.add(temp);
+        }
+        //Collections.sort(output);
         return output;
     }
 }
